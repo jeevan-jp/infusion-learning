4b19003a7f871e9c865dad2e00daeb26846f65f0
jeevan-jp <jiwanprakash65@gmail.com>
2018-06-15 01:00:27 +0530

    Infusion listeners

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 fluid.defaults("theNamespace.helloWorld", {
     gradeNames: ["fluid.component"],
+    listeners: {
+        "onCreate.onn": "{that}.sayHello"
+    },
     invokers: {
         // Creates a function on the component
         // referred to by name 'sayHello'
@@ -11,21 +14,16 @@ fluid.defaults("theNamespace.helloWorld", {
             args: ["Hello, World!"]
         }
     }
-    // listeners: {
-    //     "onCreate.logMessage": {
-    //         func: "fluid.log",
-    //         args: "My tiny component has started up"
-    //     }
-    // }
 });
 
+theNamespace.helloWorld.consoleHello = (message) => {
+    console.log(message);
+    $('.flc-messageArea').html(`<h3 style="color:red;">${message}</h3>`);
+}
+
 $(document).ready(() => {
-    theNamespace.helloWorld.consoleHello = (message) => {
-        console.log(message);
-        $('.flc-messageArea').html(`<h3>${message}</h3>`);
-    }
-    theNamespace.helloWorld({}).sayHello();
-})
+    theNamespace.helloWorld({});
+});
 // Enable visible log messages in the console
 // fluid.setLogging(true);
 // Create an instance of the component
